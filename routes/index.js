5428b7c417cbf4f5af0633a26083203f261f69e4
koralm <koralm.for@gmail.com>
2015-12-05 03:33:44 +0100

    Menu działa, ogarnac dodawanie użytkowników i logowanie

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.locals.path = req.path;
   res.render('index', { title: 'Express' });
 });
 
-router.get('/navbar', function(req, res, next) {
-  res.render('navbar', { title: 'Express' });
+router.get('/logowanie', function(req, res, next) {
+  res.render('logowanie', { title: 'Logowanie' });
+});
+
+router.get('/rejestracja', function(req, res, next) {
+  res.render('rejestracja', { title: 'rejestracja' });
+});
+
+router.get('/informacje', function(req, res, next) {
+  res.render('informacje', { title: 'informacje' });
 });
 
 module.exports = router;
